0561fa48a7d6a56583f0d150c10ffe600ece1e04
Sire65 <ha-joko@web.de>
2026-08-22 19:11:33 +0200

    Add image approval gate for playable cases

---
+++ image-approvals.js
@@ -0,0 +1,6 @@
+window.KC_KITCHEN_DETECTIVE_APPROVED_ASSETS = {
+  version: '1.0.0',
+  approvedSingle: ['KD-001'],
+  approvedDifference: [],
+  note: 'Nur visuell und logisch abgenommene Bilder werden im Spiel freigeschaltet.'
+};
